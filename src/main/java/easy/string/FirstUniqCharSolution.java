d5028275ae262cd8e2715d65f807698c09f20e9e
meidl <meidanlong@qq.com>
2018-12-11 17:01:15 +0800

    更新了回文字符串和平衡二分搜索数

--- src/main/java/easy/string/FirstUniqCharSolution.java
+++ src/main/java/easy/string/FirstUniqCharSolution.java
@@ -23,47 +23,50 @@ import java.util.TreeMap;
  * 注意事项：您可以假定该字符串只包含小写字母。
  */
 public class FirstUniqCharSolution {
-    private class Node{
-        char zifu ;
-        int weizhi;
-        public Node(char zifu,int weizhi){
-            this.zifu = zifu;
-            this.weizhi = weizhi;
-        }
-    }
 
     public int firstUniqChar(String s) {
+
         if(s.length() == 0)
             return -1;
         if(s.length() == 1)
             return 0;
-        char[] arr = s.toCharArray();
-        int index = 0;
-        HashMap<Character,Integer> map1 = new HashMap<>();
-        HashMap<Integer,Integer> map2 = new HashMap<>();
-        for(int i=0; i<arr.length; i++){
-            if (map1.containsKey(arr[i])){
-                map2.remove(map1.remove(arr[i]));
+
+        char[] chars = s.toCharArray();
+        int[] ints = new int[chars.length];
+        HashMap<Character,Integer> hashMap = new HashMap<>();
+        for(int i=0; i<chars.length; i++){
+            if(hashMap.containsKey(chars[i])){
+                ints[hashMap.get(chars[i])] = -1;
+                ints[i] = -1;
             }else{
-                map1.put(arr[i],i);
-                map2.put(map1.get(arr[i]),index++);
+                hashMap.put(chars[i],i);
             }
         }
-        if(map2.size() < arr.length){
-            for(int i : map2.keySet())
-                //第一个不重复的
+        for(int i=0; i<ints.length; i++){
+            if(ints[i] == 0)
                 return i;
-            //说明都是重复的
-            return -1;
-        }else{
-            //说明没有重复的
-            return 0;
         }
+        return -1;
+    }
+
+    public int firstUniqChar1(String s) {
+        if(s.length() == 0)
+            return -1;
+        if(s.length() == 1)
+            return 0;
+        int res = -1;
+        for(char i ='a';i<='z';i++){
+            int index = s.indexOf(i);
+            if(index!=-1 && index == s.lastIndexOf(i)){
+                res = (res==-1 || res > index) ? index : res;
+            }
+        }
+        return res;
     }
 
     public static void main(String[] args) {
-        String s = "abcdea";
+        String s = "cc";
         FirstUniqCharSolution firstUniqCharSolution = new FirstUniqCharSolution();
-        System.out.println(firstUniqCharSolution.firstUniqChar(s));
+        System.out.println(firstUniqCharSolution.firstUniqChar1(s));
     }
 }
--- src/main/java/easy/string/IsPalindromeSolution.java
+++ src/main/java/easy/string/IsPalindromeSolution.java
@@ -21,7 +21,59 @@ package easy.string;
 public class IsPalindromeSolution {
 
     public boolean isPalindrome(String s) {
+        if(null==s||s.length()==1)
+            return true;
+        int head = 0;
+        int tail = s.length()-1;
+        char[] arr = s.toCharArray();
+        while (head<tail){
+            while(head<=tail){
+                if((48<=arr[head]&&arr[head]<=57)||(65<=arr[head]&&arr[head]<=90)||(97<=arr[head]&&arr[head]<=122)){
+                    break;
+                }else{
+                    head++;
+                }
+            }
+            while(head<=tail){
+                if((48<=arr[tail]&&arr[tail]<=57)||(65<=arr[tail]&&arr[tail]<=90)||(97<=arr[tail]&&arr[tail]<=122)){
+                    break;
+                }else{
+                    tail--;
+                }
+            }
 
+            if(head>tail){
+                return true;
+            }else if((48<=arr[head]&&arr[head]<=57)&&(48<=arr[tail]&&arr[tail]<=57)){
+                if(arr[head]==arr[tail]){
+                    head++;
+                    tail--;
+                }else{
+                    return false;
+                }
+            }else if(((65<=arr[head]&&arr[head]<=90)||(97<=arr[head]&&arr[head]<=122))&&((65<=arr[tail]&&arr[tail]<=90)||(97<=arr[tail]&&arr[tail]<=122))){
+                if(arr[head]==arr[tail]||arr[head]==arr[tail]+32||arr[head]+32==arr[tail]){
+                    head++;
+                    tail--;
+                }else
+                    return false;
+            }else{
+                return false;
+            }
+        }
         return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("a: "+(int)'a');
+        System.out.println("A: "+(int)'A');
+        System.out.println("z: "+(int)'z');
+        System.out.println("Z: "+(int)'Z');
+        System.out.println("1: "+(int)'1');
+        System.out.println("0: "+(int)'0');
+        System.out.println("9: "+(int)'9');
+
+        IsPalindromeSolution isPalindromeSolution = new IsPalindromeSolution();
+        System.out.println(isPalindromeSolution.isPalindrome(",."));
     }
 }
